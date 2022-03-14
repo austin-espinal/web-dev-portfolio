de7a6722caa0b3b2d96ddf893426f77834b5aef3
austin espinal <austinespi@gmail.com>
2022-03-13 22:55:13 -0400

    fixed error with resume download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import myResume from "../../assets/web-dev-resume.pdf"
 
 function Resume() {
     return (
         <div>
             <h2 className="mb-1 ml-1">Resume</h2>
-            <p className="ml-2">To download my resume, please click here: <a src='../../../public/Web Dev Resume.docx'>My Resume</a></p>
+            <p className="ml-2">To download my resume, please click here: <a href={myResume} download ="Austin Espinal's Resume">My Resume</a></p>
             <h3 className="mt-2 ml-1">Front-end Proficiencies:</h3>
             <ul className="ml-2">
                 <li>HTML</li>
